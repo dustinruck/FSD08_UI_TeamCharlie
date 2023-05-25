d8afa6694718e406657b6c45c29bb0162f8b64a8
Dustin Ruck <dustin.ruck@gmail.com>
2023-05-25 18:17:34 -0400

    card styling added in js and css but not working yet

--- scripts/scratch_JS_Dustin.js
+++ scripts/scratch_JS_Dustin.js
@@ -1,6 +1,12 @@
 // js for the blackjack.html file
 
-// global variables
+
+class Card {
+    constructor(value, suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+}
 
 let deck = [];
 let playerHand = [];
@@ -12,30 +18,101 @@ let betAmount = 0;
 
 // functions
 
-$(document).ready(function() {
+
 function createDeck() {
     let cardNums = ["A", "K", "Q", "J", "10", "9", "8", "7",
         "6", "5", "4", "3", "2"];
     let suits = ["♥︎", "♦︎", "♠︎", "♣︎"];
     for (let i = 0; i < cardNums.length; i++) {
         for (let j = 0; j < suits.length; j++) {
-            let card = {
-                cardNum: cardNums[i],
-                suit: suits[j]
-            };
+            let card = new Card(cardNums[i], suits[j]);
+
             deck.push(card);
         }
     }
     return deck;
 }
-});
 
 function shuffleDeck() {
     for (let i = 0; i < deck.length; i++) {
         let randomCard = Math.floor(Math.random() * deck.length);
-        let temp = deck[i]; 
+        let temp = deck[i];
         deck[i] = deck[randomCard];
         deck[randomCard] = temp;
     }
     return deck;
 }
+
+function dealCard(hand, containerId) {
+    let card = deck.pop();
+    hand.push(card);
+    displayCard(card, containerId);
+    return card;
+}
+
+function displayCard(card, containerId) {
+    let cardElement = `<div class="card ${card.suit == '♠︎' || card.suit == '♣︎' ? 'black' : 'red'}">
+        <span class="top-left">${card.value}<br>${card.suit}</span>
+        <span class="bottom-right">${card.value}<br>${card.suit}</span>
+    </div>`;
+
+    $(`#${containerId}`).append(cardElement);
+}
+
+
+$(document).ready(function () {
+
+    // click rules button to display and hide the rules
+    let rulesText = document.getElementById('rulesText');
+    document.getElementById('showRulesButton').addEventListener('click', function () {
+        if (rulesText.style.display === 'block') {
+            rulesText.style.display = 'none';
+        } else {
+            rulesText.style.display = 'block';
+        }
+    });
+
+    $('#newGameButton').click(function () {
+        // Clean up the table
+        $('#dealerCard').empty();
+        $('#playerCard').empty();
+
+        // Prepare for a new game
+        $('#newGameButton').prop('disabled', true);
+        $('#hitButton').prop('disabled', false);
+        $('#standButton').prop('disabled', false);
+        $('#cashOutButton').prop('disabled', false);
+        createDeck();
+        shuffleDeck();
+        dealerHand = [];
+        playerHand = [];
+
+        // Deal the first two cards
+        dealCard(dealerHand, 'dealerCard');
+        dealCard(playerHand, 'playerCard');
+
+        // Hide dealer's card
+        $('#dealerCard .card').last().addClass('hidden');
+
+        // Deal the second two cards
+        dealCard(dealerHand, 'dealerCard');
+        dealCard(playerHand, 'playerCard');
+    });
+
+
+    $('#hitButton').click(function () {
+        dealCard(playerHand, 'playerCard');
+        let playerTotal = getTotal(playerHand);
+        if (playerTotal > 21)
+            endGame(false, "YOU'RE' OVER 21!");
+    })
+
+    $('#standButton').click(function () {
+        $('#hitButton').prop('disabled', true);
+        $('#standButton').prop('disabled', true);
+        $('#dealerCard .card').last().removeClass('hidden');
+        dealersTurn();
+
+    })
+
+    
