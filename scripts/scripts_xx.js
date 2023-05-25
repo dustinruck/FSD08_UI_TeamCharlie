66f0eec378c1e1d45c300b8fdb1c9b514b552681
xiaoxing pan <124628809+xiaoxingpan@users.noreply.github.com>
2023-05-25 19:17:10 -0400

    Update scripts_xx.js
    add getcardvalue function and get sumfunction

--- scripts/scripts_xx.js
+++ scripts/scripts_xx.js
@@ -1,14 +1,33 @@
 
+// js for the blackjack.html file
+
+
+class Card {
+    constructor(value, suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+}
+
+let deck = [];
+let playerHand = [];
+let dealerHand = [];
+let playerScore = 0;
+let dealerScore = 0;
+let playerBalance = 100;
+let betAmount = 0;
+
+// functions
+
+
 function createDeck() {
     let cardNums = ["A", "K", "Q", "J", "10", "9", "8", "7",
         "6", "5", "4", "3", "2"];
     let suits = ["♥︎", "♦︎", "♠︎", "♣︎"];
     for (let i = 0; i < cardNums.length; i++) {
         for (let j = 0; j < suits.length; j++) {
-            let card = {
-                cardNum: cardNums[i],
-                suit: suits[j]
-            };
+            let card = new Card(cardNums[i], suits[j]);
+
             deck.push(card);
         }
     }
@@ -25,39 +44,110 @@ function shuffleDeck() {
     return deck;
 }
 
-function dealCard(hand) {
-    createDeck();
-    shuffleDeck();
+function dealCard(hand, containerId) {
     let card = deck.pop();
     hand.push(card);
+    displayCard(card, containerId);
     return card;
+}
+
+function displayCard(card, containerId) {
+    let cardElement = `<div class="card ${card.suit == "♠︎" || card.suit == "♣︎" ? "black" : "red"}">
+        <span class="top-left">${card.value}<br>${card.suit}</span>
+        <span class="bottom-right">${card.value}<br>${card.suit}</span>
+    </div>`;
+
+    $(`#${containerId}`).append(cardElement);
+}
+
+function getCardValue(card) {
+    let cardValue = 0;
+    if (card == "A") {
+        cardValue = 1;
+    } else if (card == "K" || card == "Q" || card == "J") {
+        cardValue = 10;
+    } else {
+        cardValue = parseInt(card);
+    }
+    return cardValue;
+}
+
+function getSum(card1, card2) {
+    let sumCards = 0;
+    let card1Value;
+    let card2Value = getCardValue(card2);
+    if (isNaN(card1)) {
+        card1Value = getCardValue(card1);
+        if (card1Value === 1 && card2Value !== 1) {
+            sumCards = 11 + card2Value;
+        }
+        else if (card1Value !== 1 && card2Value === 1) {
+            sumCards = 11 + card2Value;
+        }
+        else if (card1Value !== 1 && card2Value !== 1) {
+            sumCards = card1Value + card2Value;
+        }
+        else {
+            sumCards = 21;
+        }
+    } else {
+        card1Value = parseInt(card1);
+        if (card2Value === 1) {
+            if (card1Value <= 10) {
+                sumCards = 11 + card1Value;
+            } else {
+                sumCards = 1 + card1Value;
+            }
+        } else {
+            sumCards = card1Value + card2Value;
+        }
+    }
+    return sumCards;
 }
 
 $(document).ready(function () {
 
     // click rules button to display and hide the rules
-    let rulesText = document.getElementById('rulesText');
-    document.getElementById('showRulesButton').addEventListener('click', function () {
-        if (rulesText.style.display === 'block') {
-            rulesText.style.display = 'none';
+    let rulesText = document.getElementById("rulesText");
+    document.getElementById("showRulesButton").addEventListener("click", function () {
+        if (rulesText.style.display === "block") {
+            rulesText.style.display = "none";
         } else {
-            rulesText.style.display = 'block';
+            rulesText.style.display = "block";
         }
     });
 
-    $('#newGameButton').click(function () {
-        $('#newGameButton').prop('disabled', true);
-        $('#hitButton').prop('disabled', false);
-        $('#standButton').prop('disabled', false);
-        $('#cashOutButton').prop('disabled', false);
-        // display the first 2 cards
-        $('#dealerCard').append(dealCard(dealerHand).cardNum + " " + dealCard(playerHand).suit);
-        $('#dealerCard').append(dealCard(dealerHand).cardNum + " " + dealCard(playerHand).suit);
+    $("#newGameButton").click(function () {
+        // Clean up the table
+        $("#dealerCard").empty();
+        $("#playerCard").empty();
 
-        $('#playerCard').append(dealCard(playerHand).cardNum + " " + dealCard(playerHand).suit);
-        $('#playerCard').append(dealCard(playerHand).cardNum + " " + dealCard(playerHand).suit);
+        // Prepare for a new game
+        $("#newGameButton").prop("disabled", true);
+        $("#hitButton").prop("disabled", false);
+        $("#standButton").prop("disabled", false);
+        $("#cashOutButton").prop("disabled", false);
+        createDeck();
+        shuffleDeck();
+        dealerHand = [];
+        playerHand = [];
 
+        // Deal the first two cards
+        dealCard(dealerHand, "dealerCard");
+        dealCard(playerHand, "playerCard");
 
+        // Hide dealer's card
+        $("#dealerCard .card").last().addClass("hidden");
+
+        // Deal the second two cards
+        dealCard(dealerHand, "dealerCard");
+        dealCard(playerHand, "playerCard");
+
+        dealerScore = getSum(dealerHand[0].value, dealerHand[1].value);
+        console.log(dealerScore);
+
+        playerScore = getSum(playerHand[0].value, playerHand[1].value);
+        console.log(dealerScore);
 
 
 
@@ -66,4 +156,28 @@ $(document).ready(function () {
 
 
 
+    $("#hitButton").click(function () {
+        dealCard(playerHand, "playerCard");
+        let playerTotal = getTotal(playerHand);
+        if (playerTotal > 21)
+            $("#playerCard").append("<p>YOU BUSTED!</p>");
+    });
+
+    $("#standButton").click(function () {
+        $("#hitButton").prop("disabled", true);
+        $("#standButton").prop("disabled", true);
+        $("#dealerCard .card").last().removeClass("hidden");
+        dealersTurn();
+
+    })
+
+    $("#cashOutButton").click(function () {
+        $("#newGameButton").prop("disabled", false);
+        $("#hitButton").prop("disabled", true);
+        $("#standButton").prop("disabled", true);
+        $("#cashOutButton").prop("disabled", true);
+        alert("You have cashed out $" + playerBalance + ". Thanks for playing!");
+        // Reset the player's money to zero
+        playerBalance = 0;
+    })
 });
