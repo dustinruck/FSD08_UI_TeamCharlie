dfbab54c819ea385cfab5cf9f5245c287fddefd2
michaellhb <michaellhbo@gmail.com>
2023-05-25 23:49:54 -0400

    update cashout button

--- scripts/scripts_final.js
+++ scripts/scripts_final.js
@@ -260,14 +260,21 @@ $(document).ready(function () {
         dealersTurn();
 
     })
-
-    $("#cashOutButton").click(function () {
-        $("#newGameButton").prop("disabled", false);
-        $("#hitButton").prop("disabled", true);
-        $("#standButton").prop("disabled", true);
-        $("#cashOutButton").prop("disabled", true);
-        alert("You have cashed out $" + playerBalance + ". Thanks for playing!");
-        // Reset the player's money to zero
-        playerBalance = 0;
-    })
+// Cash Out Button Event Listener
+$('#cashOutButton').on('click', function() {
+    $('#cashOutTexts').text("You have cashed out $" + playerBalance + ". Thanks for playing!");
+    $('#cashOutTexts').css('display', 'block');
+    $('#cashOutButton').prop('disabled', true);
+    $('#hitButton').prop('disabled', true);
+    $('#standButton').prop('disabled', true);
+    $('#newGameButton').prop('disabled', false);
+});
+    // Reset the player's money to zero
+    playerBalance = 0;
+// New Game Button Event Listener
+$('#newGameButton').on('click', function() {
+    $('#cashOutTexts').css('display', 'none');
+    $('#cashOutButton').prop('disabled', false);
+    $('#newGameButton').prop('disabled', true);
+});
 });
